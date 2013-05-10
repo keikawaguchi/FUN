a952c0119506757c26c3ed3cce7fe410ec5038c1
Drew Harvey <drew.harvey.10@gmail.com>
2013-05-10 15:15:43 -0700

    Cleaned up Explosion.cs code

--- Assets/Scripts/Explosion.cs
+++ Assets/Scripts/Explosion.cs
@@ -3,6 +3,8 @@ using System.Collections;
 
 public class Explosion : MonoBehaviour {
 	
+	const string FIRE_UNIT_PREFAB_PATH = "Prefabs/Fire";
+	
 	public float explosionDistanceX = 5.0f;
 	public float explosionDistanceZ = 5.0f;
 	public float secondsBetweenFireSpawns = 0.5f;
@@ -10,70 +12,92 @@ public class Explosion : MonoBehaviour {
 	
 	float timeOfLastFireSpawn;
 	int numberOfFireUnitsCreatedX;
-	int numberOfFireUnitsCreatedY;
+	int numberOfFireUnitsCreatedZ;
 	
-	GameObject fire;
+	GameObject fireUnit;
 
-	// Use this for initialization
 	void Start() {
-		fire = Resources.Load("Prefabs/Fire") as GameObject;
-		if (fire == null) Debug.Log ("Fire is NULL");
+		fireUnit = Resources.Load(FIRE_UNIT_PREFAB_PATH) as GameObject;
+		if (fireUnit == null) {
+			Debug.Log ("Fire is NULL");
+		}
 		numberOfFireUnitsCreatedX = 0;
-		numberOfFireUnitsCreatedY = 0;
+		numberOfFireUnitsCreatedZ = 0;
 		timeOfLastFireSpawn = -100.0f;	// explode on first update
 	}
 	
-	// Update is called once per frame
 	void Update() {
-		if (numberOfFireUnitsCreatedX > explosionDistanceX 
-			&& numberOfFireUnitsCreatedY > explosionDistanceZ) {
-			Debug.Log ("Fire destroyed");
-			Destroy(gameObject);
-			return;
+		if (explosionIsComplete()) {
+			destoryExplosionInstance();
 		}
 		
-		if ((Time.time - timeOfLastFireSpawn) > secondsBetweenFireSpawns) {
-			timeOfLastFireSpawn = Time.time;
-			Vector3 fireUnitPos;
-			
-			if (numberOfFireUnitsCreatedX < explosionDistanceX) {
-			
-				// right
-				Debug.Log("Right explosion");
-				fireUnitPos = transform.position;
-				fireUnitPos.x += numberOfFireUnitsCreatedX * scale;
-				spawnFireUnit(fireUnitPos);
-				
-				// left
-				Debug.Log("Left explosion");
-				fireUnitPos = transform.position;
-				fireUnitPos.x -= numberOfFireUnitsCreatedX * scale;
-				spawnFireUnit (fireUnitPos);
-				
-				numberOfFireUnitsCreatedX++;
-			}
-			
-			if (numberOfFireUnitsCreatedY < explosionDistanceZ) {
-			
-				// up
-				Debug.Log("Up explosion");
-				fireUnitPos = transform.position;
-				fireUnitPos.z += numberOfFireUnitsCreatedY * scale;
-				spawnFireUnit(fireUnitPos);
-				
-				// down
-				Debug.Log("Down explosion");
-				fireUnitPos = transform.position;
-				fireUnitPos.z -= numberOfFireUnitsCreatedY * scale;
-				spawnFireUnit(fireUnitPos);
-				
-				numberOfFireUnitsCreatedY++;
-			}
+		if (isTimeToSpawnFireUnit()) {
+			spawnNewFireUnits();
 		}
 	}
 	
-	void spawnFireUnit(Vector3 position) {
-		GameObject fireUnit = Instantiate(fire) as GameObject;
-		fireUnit.transform.position = position;
+	private bool isTimeToSpawnFireUnit() {
+		return (Time.time - timeOfLastFireSpawn) > secondsBetweenFireSpawns;
+	}
+	
+	private void spawnNewFireUnits() {
+		timeOfLastFireSpawn = Time.time;
+		spawnFireUnitsInXDirection();		
+		spawnFireUnitsInZDirection();
+	}
+	
+	private void spawnFireUnitsInXDirection() {
+		Vector3 fireUnitPos;	
+		if (numberOfFireUnitsCreatedX < explosionDistanceX) {		
+			
+			// right
+			Debug.Log("Right explosion");
+			fireUnitPos = transform.position;
+			fireUnitPos.x += numberOfFireUnitsCreatedX * scale;
+			spawnFireUnit(fireUnitPos);		
+			// left
+			
+			Debug.Log("Left explosion");
+			fireUnitPos = transform.position;
+			fireUnitPos.x -= numberOfFireUnitsCreatedX * scale;
+			spawnFireUnit (fireUnitPos);
+			
+			numberOfFireUnitsCreatedX++;
+		}	
+	}
+	
+	private void spawnFireUnitsInZDirection() {
+		Vector3 fireUnitPos;
+		if (numberOfFireUnitsCreatedZ < explosionDistanceZ) {	
+			
+			// up
+			Debug.Log("Up explosion");
+			fireUnitPos = transform.position;
+			fireUnitPos.z += numberOfFireUnitsCreatedZ * scale;
+			spawnFireUnit(fireUnitPos);	
+			
+			// down
+			Debug.Log("Down explosion");
+			fireUnitPos = transform.position;
+			fireUnitPos.z -= numberOfFireUnitsCreatedZ * scale;
+			spawnFireUnit(fireUnitPos);
+			
+			numberOfFireUnitsCreatedZ++;
+		}
+	}
+	
+	private void spawnFireUnit(Vector3 position) {
+		GameObject newFireUnit = Instantiate(fireUnit) as GameObject;
+		newFireUnit.transform.position = position;
+	}
+	
+	private bool explosionIsComplete() {
+		return numberOfFireUnitsCreatedX > explosionDistanceX 
+				&& numberOfFireUnitsCreatedZ > explosionDistanceZ;
+	}
+	
+	private void destoryExplosionInstance() {
+		Debug.Log ("Fire destroyed");
+		Destroy(gameObject);
 	}
 }
