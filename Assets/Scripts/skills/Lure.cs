08971d3e933e3263972d27c239e2988b7d3126ee
Drew Harvey <drew.harvey.10@gmail.com>
2013-05-10 15:30:43 -0700

    Added LureUnit prefab and LureSkill prototype scene

--- Assets/Scripts/skills/Lure.cs
+++ Assets/Scripts/skills/Lure.cs
@@ -1,15 +1,41 @@
+// Lure.cs
+// Skill used by Temptress
+// Shoots a hook that, if the front of the hook touches an enemey, it pulls 
+// the enemy back to the location of the character that shoot the hook.
+// Created by: Drew Harvey
+
 using UnityEngine;
 using System.Collections;
 
 public class Lure : MonoBehaviour {
-
-	// Use this for initialization
+	
+	const string LURE_UNIT_PREFAB_PATH = "Prefabs/Skill_Prefabs/LureUnit";
+	
+	public float distance;
+	public float width;
+	public float speed;
+	public float coolDownInSeconds;
+	
+	private Stack lureUnitStack;
+	
+	private GameObject lureUnitPrefab;
+	
 	void Start () {
+		loadLureUnitPrefab();
+		lureUnitStack = new Stack();
+	}
+	
+	void Update () {
 	
 	}
 	
-	// Update is called once per frame
-	void Update () {
-	
+	private void loadLureUnitPrefab() {
+		lureUnitPrefab = Resources.Load(LURE_UNIT_PREFAB_PATH) as GameObject;
+		if (lureUnitPrefab == null) {
+			Debug.Log ("Lure Unit loaded unsuccessfully");
+		}
+		else {
+			Debug.Log ("Lure Unit loaded successfully");
+		}
 	}
 }
