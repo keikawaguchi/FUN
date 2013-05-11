b16f0facc2ab308305c4d337ce48854c5f38994a
Drew Harvey <drew.harvey.10@gmail.com>
2013-05-10 17:13:46 -0700

    Updated Lure.cs

--- Assets/Scripts/skills/Lure.cs
+++ Assets/Scripts/skills/Lure.cs
@@ -9,36 +9,50 @@ using System.Collections;
 
 public class Lure : MonoBehaviour {
 	
+	private enum State {
+		Extending,
+		Returning
+	}
+	
 	#region Class Members
 	const string LURE_UNIT_PREFAB_PATH = "Prefabs/Skill_Prefabs/LureUnit";
 	
 	public float distanceToTravel;
-	public float width;
-	public float speed;
-	public float coolDownInSeconds;
+	public float grabWidth;
+	public float scaleSpeed;
 	private float lureUnitSpawnDelayInSeconds;
+	private float numberOfLureUnitsToSpawn;
 	private float timeOfLastLureUnitSpawn;
 	
+	private State currentState;
 	private Stack lureUnitStack;
 	private GameObject lureUnitPrefab;
 	#endregion
 	
 	void Start () {
 		loadLureUnitPrefab();
+		currentState = State.Extending;
 		lureUnitStack = new Stack();
 	}
 	
 	void Update () {
+		if (isComplete()) {
+			destoryLureObject();
+		}
 		
+		if (isTimeToSpawnLureUnit()) {
+			
+		}
 	}
 	
+	#region Initialization Methods
 	private void loadLureUnitPrefab() {
 		lureUnitPrefab = Resources.Load(LURE_UNIT_PREFAB_PATH) as GameObject;
 		if (lureUnitPrefab == null) {
-			Debug.Log ("Lure Unit loaded unsuccessfully");
+			Debug.Log ("Lure Unit Prefab loaded unsuccessfully");
 		}
 		else {
-			Debug.Log ("Lure Unit loaded successfully");
+			Debug.Log ("Lure Unit Prefab loaded successfully");
 		}
 	}
 	
@@ -47,12 +61,39 @@ public class Lure : MonoBehaviour {
 		lureUnitSpawnDelayInSeconds = distanceToTravel / lureUnitHeight;
 	}
 	
-	private void addLureUnit() {
+	private void calculateNumberOfUnitsToSpawn() {
+		float lureUnitHeight = lureUnitPrefab.transform.localScale.z;
+		lureUnitSpawnDelayInSeconds = distanceToTravel / lureUnitHeight;
+	}
+	#endregion
+	
+	private bool isComplete() {	
+		Debug.Log("Lure skill complete");
+		return lureUnitStack.Count == 0;
+	}
+	
+	private void destoryLureObject() {
+		Debug.Log("Lure skill destoryed");
+		Destroy(gameObject);
+	}
+	
+	private bool isTimeToSpawnLureUnit() {
+		return (Time.time - timeOfLastLureUnitSpawn) > calculateLureUnitSpawnDelay;
+	}
+	
+	private bool isFullyExtended() {
+		return lureUnitStack.Count >= numberOfLureUnitsToSpawn;
+	}
+	
+	private void addLureUnit(Vector3 position) {
+		Debug.Log("Lure unit added");
 		GameObject newLureUnit = Instantiate(lureUnitPrefab) as GameObject;
+		newLureUnit.transform.position = position;
 		lureUnitStack.Push(newLureUnit);
 	}
 	
 	private void removeLureUnit() {
+		Debug.Log("Lure unit removed");
 		GameObject lureUnitToRemove = lureUnitStack.Pop() as GameObject;
 		Destroy(lureUnitToRemove);
 	}
