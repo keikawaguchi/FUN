3fed4bfeeefeec813a41e61ee8ef25a84f477fd2
Beshoy Sarkis <beshoy_sarkis@hotmail.com>
2013-05-12 00:45:17 -0700

    Added prefabs for upgrades, A LOT of cleanup, upgrade functionality still under progress

--- Assets/Scripts/UpgradePickUp.cs
+++ Assets/Scripts/UpgradePickUp.cs
@@ -3,14 +3,30 @@ using System.Collections;
 
 public class UpgradePickUp : MonoBehaviour {
 	
+	bool showing = true;
+	int upgradeType;
 	// Use this for initialization
-	void Start () {
-	
+	void Start () 
+	{
+		
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
+		upgradeType = Random.Range(1,3);
+		if( upgradeType == 1)
+		{
+			GetComponent<MeshRenderer>().material.mainTexture = Resources.Load ("Materials/SpeedUpgrade") as Texture;
+		}
+		else if( upgradeType == 2 )
+		{
+			GetComponent<MeshRenderer>().material.mainTexture = Resources.Load ("Materials/ExplosionUpgrade") as Texture;
+		}
+		else if( upgradeType == 3 )
+		{
+			GetComponent<MeshRenderer>().material.mainTexture = Resources.Load ("Materials/BombUpgrade") as Texture;
+		}
 	}
 	
 	public void OnTriggerEnter()
@@ -24,4 +40,12 @@ public class UpgradePickUp : MonoBehaviour {
 		yield return new WaitForSeconds(3);
 		renderer.enabled = true;
 	}
+	
+	public enum upgrades
+	{
+		SpeedUp = 1,
+		Explosion = 2,
+		Bomb = 3
+		
+	};
 }
--- Beshoy/README.md
+++
@@ -1,4 +0,0 @@
-FUN
-===
-
-Forsaken Universe of the North - Spring 2013 CSS 385 Final Project at UWB
