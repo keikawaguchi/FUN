9ba6dd28c21e35507c1c7ad312222404c8aad86b
Drew Harvey <drew.harvey.10@gmail.com>
2013-05-12 19:26:01 -0700

    Added public speed methods to CharacterMovement.cs
    Added increaseSpeedByPercentage(float) and
    decreaseSpeedByPercentage(float).

--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -8,10 +8,10 @@ public class CharacterMovement : MonoBehaviour {
 		CannotMove
 	}
 	
-	public float speed;
-	private Vector3 aimDirection;
+	public float speed = 100f;
+	private float speedModifier = 1f;	// 1 = 100% speed
+	private Vector3 aimDirection;		// direction for aiming skills
 	MovementState currentMovementState;
-	float newSpeed = 1;
 	
 	public void Start() {
 		currentMovementState = MovementState.CanMove;
@@ -32,15 +32,32 @@ public class CharacterMovement : MonoBehaviour {
 	public Vector3 getAimDirection() {
 		return aimDirection;
 	}
-
+	
+	public void increaseSpeedByPercentage(float percentage) {
+		if (percentage >= 10) {
+			percentage /= 10f;
+		}
+		speedModifier += percentage;
+	}
+	
+	public void decreaseSpeedByPercentage(float percentage) {
+		if (percentage >= 10) {
+			percentage /= 10f;
+		}
+		speedModifier -= percentage;
+	}
 	#endregion
 	
 	private void updateMovement() {
 		Vector3 movement;
 		movement.y = 0;
-		movement.x = Input.GetAxis("Horizontal") * speed * Time.smoothDeltaTime;
-		movement.z = Input.GetAxis("Vertical") * speed * Time.smoothDeltaTime;
-		this.transform.Translate (movement);
+		movement.x = Input.GetAxis("Horizontal") * calculateSpeed();
+		movement.z = Input.GetAxis("Vertical") * calculateSpeed();
+		this.transform.Translate(movement);
+	}
+	
+	private float calculateSpeed() {
+		return speed * speedModifier * Time.smoothDeltaTime;
 	}
 	
 	private void updateAimDirection() {
