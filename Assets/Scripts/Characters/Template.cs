925fdfd68ac399f9f51f0859fd28b0d4bc47384b
Drew Harvey <drewh@pwnengine.com>
2013-05-15 20:37:53 -0700

    Fixed Template.cs error

--- Assets/Scripts/Characters/Template.cs
+++ Assets/Scripts/Characters/Template.cs
@@ -4,7 +4,7 @@
 using UnityEngine;
 using System.Collections;
 
-public class MerliniBehavior : MonoBehaviour {
+public class Template : MonoBehaviour {
 	
 	private CharacterMovement characterMovement;
 	private Controller controller;
