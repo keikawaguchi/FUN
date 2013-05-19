269dbbeebedc3ac0f42a606e0822cd63c6d36590
Kei Kawaguchi <kristoferkawaguchi@gmail.com>
2013-05-18 21:43:25 -0700

    Added Stun text to Hammertime (need to clean up)

--- Assets/Scripts/Skills/HammerTime.cs
+++ Assets/Scripts/Skills/HammerTime.cs
@@ -58,6 +58,16 @@ public class HammerTime : MonoBehaviour {
 	void OnTriggerEnter(Collider collision) {
 		if (collision.tag == PLAYER_TAG && collision.gameObject != owner) {
 			
+			// add stun gametext
+			GameObject t = Resources.Load ("Prefabs/Text/PopupText") as GameObject;
+			GameObject text = Instantiate(t) as GameObject;	
+			
+			PopupText popupText = text.GetComponent<PopupText>();
+			popupText.initialize();
+			popupText.setDuration(HAMMER_STUN_DURATION);
+			popupText.setPosition(collision.gameObject.transform.position.x, collision.gameObject.transform.position.z + 7);
+			popupText.setPredefinedText("Stun");
+			
 			// stun hero
 			AlterSpeed alterSpeed;
 			alterSpeed = collision.gameObject.AddComponent<AlterSpeed>();
@@ -85,6 +95,16 @@ public class HammerTime : MonoBehaviour {
 				float distance = Vector3.Distance(collisionPos, player.transform.position);
 				if (distance < HAMMER_AOE_EFFECT) {
 					setSlowOnPlayer(player);
+					
+					// add stun gametext
+					GameObject t = Resources.Load ("Prefabs/Text/PopupText") as GameObject;
+					GameObject text = Instantiate(t) as GameObject;	
+					
+					PopupText popupText = text.GetComponent<PopupText>();
+					popupText.initialize();
+					popupText.setDuration(HAMMER_STUN_DURATION);
+					popupText.setPosition(player.transform.position.x, player.transform.position.z + 7);
+					popupText.setPredefinedText("Stun");
 				}
 			}
 			
