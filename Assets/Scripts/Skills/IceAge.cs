580471ae95abb73951da9b5c9d3596c9b81c2f5d
Drew Harvey <drew.harvey.10@gmail.com>
2013-05-19 17:40:49 -0700

    Fixed iceage compile error

--- Assets/Scripts/Skills/IceAge.cs
+++ Assets/Scripts/Skills/IceAge.cs
@@ -4,18 +4,18 @@ using System.Collections;
 public class IceAge : MonoBehaviour {
 	private const string ICEAGE_PREGAB_PATH = "Prefabs/Skills/IceAge";
 	
+	private GameObject iceAgePrefab;
 	private GameObject iceAgeObj;
 	private IndestructubleWall iceAge;
 	private Vector3 iceAgePos;
 	
 	// Use this for initialization
 	void Start () {
-	
+		iceAgePrefab =  Resources.Load(ICEAGE_PREGAB_PATH) as GameObject;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		iceAgeObj = Instantiate (ICEAGE_PREGAB_PATH) as GameObject;
-		
+		iceAgeObj = Instantiate(iceAgePrefab) as GameObject;	
 	}
 }
