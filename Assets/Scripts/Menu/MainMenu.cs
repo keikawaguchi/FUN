4e7fd2334919fba53f997560ab218fb46fbec5aa
BeshoySarkis <beshoy_sarkis@hotmail.com>
2013-05-27 16:58:07 -0700

    Menu, under construction

--- Assets/Scripts/Menu/MainMenu.cs
+++ Assets/Scripts/Menu/MainMenu.cs
@@ -8,6 +8,12 @@ public class MainMenu : MonoBehaviour {
 	public ComboBox Player2;
 	public ComboBox Player3;
 	public ComboBox Player4;
+	
+	GUIContent[] teamList;
+	public ComboBox player1Team;
+	public ComboBox player2Team;
+	public ComboBox player3Team;
+	public ComboBox player4Team;
 	private GUIStyle listStyle = new GUIStyle();
 	
 	float originalWidth = 800;
@@ -32,6 +38,13 @@ public class MainMenu : MonoBehaviour {
 		comboBoxList[1] = new GUIContent("Fanndis");
 		comboBoxList[2] = new GUIContent("Merlini");
 		comboBoxList[3] = new GUIContent("Temptress");
+		
+		teamList = new GUIContent[3];
+		teamList[0] = new GUIContent("Solo");
+		teamList[1] = new GUIContent("Team 1");
+		teamList[2] = new GUIContent("Team 2");
+		
+		
  		var svMat = GUI.matrix; // save current matrix
 		listStyle.normal.textColor = Color.white; 
 		listStyle.onHover.background =
@@ -41,15 +54,9 @@ public class MainMenu : MonoBehaviour {
 		listStyle.padding.top =
 		listStyle.padding.bottom = 4;
  
-		Player1 = new ComboBox(new Rect(15, 170, 150, 20), comboBoxList[0], comboBoxList, "button", "box", listStyle);
-		Player2 = new ComboBox(new Rect(15,470,150, 20), comboBoxList[1], comboBoxList, "button", "box", listStyle);
-		Player3 = new ComboBox(new Rect(635,170,150, 20), comboBoxList[2], comboBoxList, "button", "box", listStyle);
-		Player4 = new ComboBox(new Rect(635,470,150, 20), comboBoxList[3], comboBoxList, "button", "box", listStyle);
+		ComboBoxes();
 		
-		Player1.setIndex(0);
-		Player2.setIndex(1);
-		Player3.setIndex(2);
-		Player4.setIndex(3);
+		
 		GUI.matrix = svMat;
 	}
 	
@@ -64,15 +71,50 @@ public class MainMenu : MonoBehaviour {
     	scale.z = 1;
     	var svMat = GUI.matrix; // save current matrix
 		GUI.matrix = Matrix4x4.TRS(Vector3.zero, Quaternion.identity, scale);
-		GUI.Box(new Rect(15,20,150,140),"Player 1");
-		GUI.Box(new Rect(15,320,150,140),"Player 2");
-		GUI.Box(new Rect(635,20,150,140),"Player 3");
-		GUI.Box(new Rect(635,320,150,140),"Player 4");
 		
-		GUI.Box(new Rect(185,20,430,170),"Champions");
-		GUI.Box(new Rect(185,225,430,270),"Description");
+		PlayerSelections();
+		Descriptions();
 		
-		if(GUI.Button(new Rect(190,60,100,100),"Albion"))
+		Player1.Show();
+		Player2.Show();
+		Player3.Show();
+		Player4.Show();
+		
+		player1Team.Show();
+		player2Team.Show();
+		player3Team.Show();
+		player4Team.Show();
+		GUI.matrix = svMat;
+	}
+	
+	private void ComboBoxes()
+	{
+		Player1 = new ComboBox(new Rect(156,185,100,20), comboBoxList[0], comboBoxList, "button", "box", listStyle);
+		Player2 = new ComboBox(new Rect(281,185,100,20), comboBoxList[1], comboBoxList, "button", "box", listStyle);
+		Player3 = new ComboBox(new Rect(406,185,100,20), comboBoxList[2], comboBoxList, "button", "box", listStyle);
+		Player4 = new ComboBox(new Rect(531,185,100,20), comboBoxList[3], comboBoxList, "button", "box", listStyle);
+		Player1.setIndex(0);
+		Player2.setIndex(1);
+		Player3.setIndex(2);
+		Player4.setIndex(3);
+		
+		player1Team = new ComboBox(new Rect(156,25,100,20),teamList[0],teamList,"button","box",listStyle);
+		player2Team = new ComboBox(new Rect(281,25,100,20),teamList[0],teamList,"button","box",listStyle);
+		player3Team = new ComboBox(new Rect(406,25,100,20),teamList[0],teamList,"button","box",listStyle);
+		player4Team = new ComboBox(new Rect(531,25,100,20),teamList[0],teamList,"button","box",listStyle);
+		player1Team.setIndex(0);
+		player2Team.setIndex(0);
+		player3Team.setIndex(0);
+		player4Team.setIndex(0);
+	}
+	
+	private void Champions()
+	{
+	}
+	
+	private void Descriptions()
+	{		
+		if(GUI.Button(new Rect(5,300,50,50),"Albion"))
 		{
 			selectedChamp = "Albion-The Hunter";
 			Description = "In a big city which still held by the ordinary men, a scientist, though mutation of the his brain and mind,"+
@@ -90,7 +132,7 @@ public class MainMenu : MonoBehaviour {
 				"the champion gets stuned for 3 seconds. \nCooldown: 10 seconds";
 			SkillTwoDisc = "Albion teleports 3 squares to the front direction of the hero. \nCooldown: 10 seconds";
 		}
-		if(GUI.Button(new Rect(296,60,100,100),"Fanndis"))
+		if(GUI.Button(new Rect(60,300,50,50),"Fanndis"))
 		{
 			selectedChamp = "Fanndis-The Ice Queen";
 			Description = "For thousand years, the Ice Tribe which was ruled by the beautiful queen - Fanndis. The ice queen lived on " +
@@ -109,7 +151,7 @@ public class MainMenu : MonoBehaviour {
 			SkillTwoDisc = "Fanndis compresses the water vapor in the air to an impassible wall of ice to block all movement and explosion." +
 				"\nCooldown: 3 seconds";
 		}
-		if(GUI.Button(new Rect(403,60,100,100),"Merlini"))
+		if(GUI.Button(new Rect(5,355,50,50),"Merlini"))
 		{
 			selectedChamp = "Merlini-The Magician";
 			Description = "Merlini, although he is completely aware of his magical power, isn't quite sure where he came from." +
@@ -131,7 +173,7 @@ public class MainMenu : MonoBehaviour {
 			 	"any nearby opponent for 2 seconds. \nCooldown: 10 seconds";
 			SkillTwoDisc = "Merlini places a standard bomb below every player of the map. \nCooldown: 25 seconds";
 		}
-		if(GUI.Button(new Rect(509,60,100,100),"Temptress"))
+		if(GUI.Button(new Rect(60,355,50,50),"Temptress"))
 		{
 			selectedChamp = "Temptress-The Misguided";
 			Description = "Integration of the embodiment of evil and elegant, Temptress playing with " +
@@ -147,24 +189,26 @@ public class MainMenu : MonoBehaviour {
 			SkillOneDisc = "Temptress pulls an opponent within 5 units to your current location. \nCooldown: 5 seconds";
 			SkillTwoDisc = "Temptress applies slow-down for 2 seconds to nearest enemy in 3 squares. \nCooldown: 10 seconds";
 		}
-		GUI.Label(new Rect(190,250,200,100),selectedChamp);
 		
-		GUILayout.BeginArea(new Rect(190,270,425,125));
-		scrollPosition = GUILayout.BeginScrollView(scrollPosition,GUILayout.Width(425),GUILayout.Height(125));
+		GUILayout.BeginArea(new Rect(125,325,500,300));
+		scrollPosition = GUILayout.BeginScrollView(scrollPosition,GUILayout.Width(500),GUILayout.Height(300));
 		GUI.skin.box.wordWrap = true;
 		GUILayout.Box(Description);
 		GUILayout.EndScrollView();
 		GUILayout.EndArea();
 		
-		//GUI.Label(new Rect(190,270,425,125),Description);
-		GUI.Label(new Rect(190,400,200,100),SkillOne);
-		GUI.Label(new Rect(400,400,200,100),SkillTwo);
-		GUI.Label(new Rect(190,420,200,100),SkillOneDisc);
-		GUI.Label(new Rect(400,420,200,100),SkillTwoDisc);
-		Player1.Show();
-		Player2.Show();
-		Player3.Show();
-		Player4.Show();
-		GUI.matrix = svMat;
+		GUI.Label(new Rect(128,300,200,100),selectedChamp);
+		GUI.Label(new Rect(600,300,200,100),SkillOne);
+		//GUI.Label(new Rect(400,400,200,100),SkillTwo);
+		//GUI.Label(new Rect(190,420,200,100),SkillOneDisc);
+		//GUI.Label(new Rect(400,420,200,100),SkillTwoDisc);
+	}
+	
+	private void PlayerSelections()
+	{
+		GUI.Box(new Rect(156,120,100,60),"Player 1");
+		GUI.Box(new Rect(281,120,100,60),"Player 2");
+		GUI.Box(new Rect(406,120,100,60),"Player 3");
+		GUI.Box(new Rect(531,120,100,60),"Player 4");
 	}
 }
