863a8d0cdc95ef6d0013f2d6d2a910c9e09375c7
Drew Harvey <drew.harvey.10@gmail.com>
2013-05-27 18:25:02 -0700

    Updated animation script for proper alignment

--- Assets/Scripts/Animations/Animation.cs
+++ Assets/Scripts/Animations/Animation.cs
@@ -5,23 +5,43 @@ public class Animation : MonoBehaviour {
 	
 	public GameObject objectToFollow;
 	
+	private Vector3 customAnimationOffset;
+	
+	
 	void Start() {
 		if (objectToFollow == null) {
 			return;
 		}
 		transform.forward = objectToFollow.transform.forward;
+		customAnimationOffset = new Vector3(0, 20, 0);
 	}
 
 	void Update () {
 		if (objectToFollow == null) {
-			Destroy(gameObject);
-			return;
+			destroyAnimation();
 		}
-		transform.position = objectToFollow.transform.position;
-		transform.position = new Vector3(transform.position.x, 20, transform.position.z);
+		else {
+			updatePosition();	
+		}
 	}
 	
 	public void attachToObject(GameObject obj) {
 		objectToFollow = obj;
+	}
+	
+	private void destroyAnimation() {
+		Destroy(gameObject);
+	}
+	
+	private void updatePosition() {
+		float halfAnimationHeight = transform.localScale.z / 2.0f;
+		float halfFollowingHeight = objectToFollow.transform.localScale.z / 2.0f;
+		
+		// Position so that animation bottom lines up with objects bottom
+		float zOffset = halfAnimationHeight - halfFollowingHeight;
+		
+		// Apply position offsets
+		transform.position = objectToFollow.transform.position;
+		transform.position += new Vector3(0, 0, zOffset) + customAnimationOffset;	
 	}
 }
