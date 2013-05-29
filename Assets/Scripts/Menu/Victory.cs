d66115d6b43cabb7c8e0b595496dc70d13a0cda5
Kei Kawaguchi <kristoferkawaguchi@gmail.com>
2013-05-29 13:55:24 -0700

    Victory scene has back button!

--- Assets/Scripts/Menu/Victory.cs
+++ Assets/Scripts/Menu/Victory.cs
@@ -6,7 +6,9 @@ public class Victory : MonoBehaviour {
 	private GameManager manager;
 	private GUIText winner;
 	private string winnerName;
-	
+	float originalWidth = 800;
+	float originalHeight = 600;
+	Vector3 scale;
 	// Use this for initialization
 	void Start () {
 		GameObject managerObj = GameObject.Find ("Game Manager");
@@ -17,8 +19,14 @@ public class Victory : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		scale.x = Screen.width/originalWidth; // calculate hor scale
+    	scale.y = Screen.height/originalHeight; // calculate vert scale
+    	scale.z = 1;
+    	var svMat = GUI.matrix; // save current matrix
+		GUI.matrix = Matrix4x4.TRS(Vector3.zero, Quaternion.identity, scale);
 		winnerName = manager.getWinner ();
 		winner.text = winnerName;
+		GUI.matrix = svMat;
 	}
 	
 	private void loadText() {
