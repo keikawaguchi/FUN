235ba2e891b6e77623b50279807537b3576f0a6d
calvinhsieh <calvin5231@gmail.com>
2013-06-06 15:51:25 -0700

    Updated champion selection menu

--- Assets/Scripts/Champions/ChampInfo.cs
+++ Assets/Scripts/Champions/ChampInfo.cs
@@ -1,29 +1,135 @@
 using UnityEngine;
 using System.Collections;
+using System.IO;
+using System.Text.RegularExpressions;
 
 public class ChampInfo : MonoBehaviour {
+	// champion texture path
+	private const string CHAMP_BOX_IMAGE_PATH = "Textures/Menu/ChampBox";
+	private const string ALBION_IMAGE_PATH = "Textures/Champions/AlbionIcon";
+	private const string FANNDIS_IMAGE_PATH = "Textures/Champions/FanndisIcon";
+	private const string KIRITO_IMAGE_PATH = "Textures/Champions/KiritoIcon";
+	private const string MERLINI_IMAGE_PATH = "Textures/Champions/MerliniIcon";
+	private const string TEMPTRESS_IMAGE_PATH = "Textures/Champions/TemptressIcon";
+	
+	private int TOTAL_AVAILABLE_CHAMPS = 5;
+	private int MAX_UNIQUE_SKILLS = 2;
+	
+//	private TextAsset champFile;
+	private Texture2D[] champTextures;
 	private string[] champNames;
+//	private string[] skillsNames;
+//	private string[] skillsDescrip;
+//	private int [] skillsCDs;
+	private int index;
 	
 	// Use this for initialization
 	void Start () {
-	
+		initializeVariables ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		champNames[0] = "Albion";
-		champNames[1] = "Fanndis";
-		champNames[2] = "Kirito";
-		champNames[3] = "Merlini";
-		champNames[4] = "Temptress";
+		
+	}
+	
+//	public void constructChampInfo(int index) {
+//		champFile = loadChampFile (index);
+//		StringReader reader;
+//		if (champFile.text == null)
+//			Debug.Log ("it's null");
+//		reader = new StringReader(champFile.text);
+//		if (reader == null) {
+//			Debug.Log ("File not found or not readable");
+//			return;
+//		}
+//		
+//		loadTexture (champFile, index);
+//	}
+//	
+	public void setCurrentIndex(int index) {
+		this.index = index;
+	}
+	
+	public int getCurrentIndex() {
+		return index;
+	}
+	
+	public Texture2D getChampTexture(int index) {
+		return champTextures[index];
+	}
+	
+	public void showChampTexture(Rect rect, int index) {
+		
+		GUI.Box (rect, champTextures[index]);
 	}
 	
 	public string getChampName(int index) {
 		return champNames[index];
 	}
 	
-//	public string getSkillDescription(string championName) {
-//		if (championName == "Albion")
-			
+	public string getSkillDescription(int index) {
+		switch(index) {
+		case 1:
+			return "Holy Trap - CD: 10 sec\n" +
+				"Albion places a trap on the map that is visible for 1 second " +
+				"by everyone, to stun whoever walks through it except the hero places the trap.\n" +
+				"Holy Blink - CD: 10 sec\n" +
+				"Albion teleports 3 squares to the front direction of the hero.";
+		default:
+			return "No skills";
+		}
+	}
+//	
+	public void initializeVariables() {
+		champTextures = new Texture2D[TOTAL_AVAILABLE_CHAMPS + 1];
+		champNames = new string[TOTAL_AVAILABLE_CHAMPS + 1];
+//		skillsNames = new string[MAX_UNIQUE_SKILLS];
+//		skillsDescrip = new string[MAX_UNIQUE_SKILLS];
+//		skillsCDs = new int[MAX_UNIQUE_SKILLS];
+		
+		index = 1;  // 0 is not used
+		
+		setChampNames();
+		
+	}
+//	
+	private void setChampNames() {
+		champNames[1] = "Albion";
+		champNames[2] = "Fanndis";
+		champNames[3] = "Kirito";
+		champNames[4] = "Merlini";
+		champNames[5] = "Temptress";
+	}
+	
+	private void setTextures() {
+		champTextures[0] = Resources.Load (CHAMP_BOX_IMAGE_PATH) as Texture2D;
+		champTextures[1] = Resources.Load (ALBION_IMAGE_PATH) as Texture2D;
+		champTextures[2] = Resources.Load (FANNDIS_IMAGE_PATH) as Texture2D;
+		champTextures[3] = Resources.Load (KIRITO_IMAGE_PATH) as Texture2D;
+		champTextures[4] = Resources.Load (MERLINI_IMAGE_PATH) as Texture2D;
+		champTextures[5] = Resources.Load (TEMPTRESS_IMAGE_PATH) as Texture2D;
+	}
+//	
+//	private TextAsset loadChampFile(int index) {
+////		string mapToLoad = maps [mapID];
+////		if (mapToLoad == null) {
+////			Debug.Log ("MapID " + mapID + " does not exist.");
+////		}	
+////		return Resources.Load (mapToLoad, typeof(TextAsset)) as TextAsset;
+//		
+//		string fileToLoad = "Resources/Champions Info/" + champNames[index];
+//		Debug.Log ("File path: " + fileToLoad);
+//		return Resources.Load (fileToLoad, typeof(TextAsset)) as TextAsset;
+//	}
+//	
+//	private void loadTexture(TextAsset file, int index) {
+//		Match match;
+//	
+//		match = Regex.Match(file.text, @"Path:(.*)");
+//		if (match.Success) {
+//			champTexture = Resources.Load(match.Groups[1].ToString()) as Texture2D;
+//			Debug.Log("Champion Texture: " + match.Groups[1].ToString ());
+//		}
 //	}
 }
--- Assets/Scripts/Menu/ChampSelection.cs
+++ Assets/Scripts/Menu/ChampSelection.cs
@@ -23,7 +23,9 @@ public class ChampSelection : MonoBehaviour {
 	private const string CONFIRM_IMAGE_PATH = "Textures/Menu/Confirm";
 	private const string CONFIRMED_IMAGE_PATH = "Textures/Menu/Confirmed";
 	
+	private const int MAX_TEAM_TYPES = 3;
 	private const int MAX_PLAYERS = 4;
+	private const int TOTAL_AVAILABLE_CHAMPS = 5;
 	
 	// Fonts
 	public Font titleFont;
@@ -31,22 +33,15 @@ public class ChampSelection : MonoBehaviour {
 	
 	// GUIStyle
 	private GUIStyle titleStyle;
-	private GUIStyle playerTabStyle;
+	private GUIStyle playerTagStyle;
 	private GUIStyle bodyStyle;
 	private GUIStyle teamTagStyle;
 	
-	// texture
-	private Texture2D solo;
-	private Texture2D team1;
-	private Texture2D team2;
-	
-	// champion texture
-	private Texture2D champBox;
-	private Texture2D albionIcon;
-	private Texture2D fanndisIcon;
-	private Texture2D kiritoIcon;
-	private Texture2D merliniIcon;
-	private Texture2D temptressIcon;
+	// selected info
+	private Texture2D[] teamTexture;
+	private Texture2D[] champTextures;
+	private string[] champNames;
+	private string[] champSkillsDescrip;
 	
 	private Texture2D confirm;
 	private Texture2D confirmed;
@@ -62,28 +57,47 @@ public class ChampSelection : MonoBehaviour {
 	// player selection info
 	private bool[] isPlayerInRoom;  // is the player in the room?
 	private bool[] confirmSelection;  // player confirmed champion selection
+	private bool[] navigateLeft;
+	private bool[] navigateRight;
+	private bool[] navigateUp;
+	private bool[] navigateDown;
+	private bool[] defaultChampViewed;
 	private string[] playersSelectedChamps;  // the selected champion to be played
 	
-//	private ChampInfo champInfo;
+	private ChampInfo champInfo;
+	
+	// current selected info
+	private int[] currentSelectedIndex;
+	private Texture2D[] defaultChampTextureDisplayed;
+	private string[] defaultChampNameDisplayed;
 	
 	// Use this for initialization
 	void Start () {
-		instantiateVariables ();
+		initializeVariables ();
 		
 		loadScripts ();
 		loadTextures ();
 //		loadButtons ();
 		
 		setControllers ();
+		
+		// construct champions info
+		setChampNames ();
+		setSkillsDescrip ();
+		
+		setDefaultChampInfo ();
 	}
 	
 	void Update() {
-		setPlayerJoined ();
-		setSelectionConfirmed ();
+		
 	}
 	
 	private void OnGUI() {
-		if (titleFont != null && bodyFont != null && champBox != null) {
+		setPlayerJoined ();
+		setSelectionConfirmed ();
+		navigateState ();
+		
+		if (titleFont != null && bodyFont != null) {
 			setGUIStyle ();
 		
 			// begin scaling the contents
@@ -93,16 +107,16 @@ public class ChampSelection : MonoBehaviour {
 	    	var saveMatrix = GUI.matrix;  // save current matrix
 			GUI.matrix = Matrix4x4.TRS(Vector3.zero, Quaternion.identity, scale);
 			
-			displayContents ();
+			displayLayout ();
 			
 			GUI.matrix = saveMatrix;
 			// end scaling the contents
 		}
 	}
 	
-	private void instantiateVariables() {
+	private void initializeVariables() {
 		titleStyle = new GUIStyle();
-		playerTabStyle = new GUIStyle();
+		playerTagStyle = new GUIStyle();
 		bodyStyle = new GUIStyle();
 		teamTagStyle = new GUIStyle();
 		
@@ -110,33 +124,74 @@ public class ChampSelection : MonoBehaviour {
 		controllers = new XInputController[MAX_PLAYERS + 1];
 		confirmSelection = new bool[MAX_PLAYERS + 1];
 		isPlayerInRoom = new bool[MAX_PLAYERS + 1];
+		defaultChampViewed = new bool[MAX_PLAYERS + 1];
+		navigateLeft = new bool[MAX_PLAYERS + 1];
+		navigateRight = new bool[MAX_PLAYERS + 1];
+		navigateUp = new bool[MAX_PLAYERS + 1];
+		navigateDown = new bool[MAX_PLAYERS + 1];
+		
+		teamTexture = new Texture2D[MAX_TEAM_TYPES];
+		champNames = new string[TOTAL_AVAILABLE_CHAMPS + 1];
+		champTextures = new Texture2D[TOTAL_AVAILABLE_CHAMPS + 1];  // 0 is for default view
+		champSkillsDescrip = new string[TOTAL_AVAILABLE_CHAMPS + 1];
+		
+		currentSelectedIndex = new int[MAX_PLAYERS + 1];
+		defaultChampTextureDisplayed = new Texture2D[MAX_PLAYERS + 1];
+		defaultChampNameDisplayed = new string[MAX_PLAYERS + 1];
 	}
 	
 	private void loadScripts() {
 		for (int i = 1; i <= MAX_PLAYERS; i++)
 			controllers[i] = gameObject.AddComponent<XInputController>();
+		
+		champInfo = gameObject.AddComponent<ChampInfo>();
 	}
 	
 	private void loadTextures() {
-		solo = Resources.Load (SOLO_IMAGE_PATH) as Texture2D;
-		team1 = Resources.Load (TEAM1_IMAGE_PATH) as Texture2D;
-		team2 = Resources.Load (TEAM2_IMAGE_PATH) as Texture2D;
+		teamTexture[0] = Resources.Load (SOLO_IMAGE_PATH) as Texture2D;
+		teamTexture[1] = Resources.Load (TEAM1_IMAGE_PATH) as Texture2D;
+		teamTexture[2] = Resources.Load (TEAM2_IMAGE_PATH) as Texture2D;
 		
-		champBox = Resources.Load (CHAMP_BOX_IMAGE_PATH) as Texture2D;
-		albionIcon = Resources.Load (ALBION_IMAGE_PATH) as Texture2D;
-		fanndisIcon = Resources.Load (FANNDIS_IMAGE_PATH) as Texture2D;
-		kiritoIcon = Resources.Load (KIRITO_IMAGE_PATH) as Texture2D;
-		merliniIcon = Resources.Load (MERLINI_IMAGE_PATH) as Texture2D;
-		temptressIcon = Resources.Load (TEMPTRESS_IMAGE_PATH) as Texture2D;
+		champTextures[0] = Resources.Load (CHAMP_BOX_IMAGE_PATH) as Texture2D;
+		champTextures[1] = Resources.Load (ALBION_IMAGE_PATH) as Texture2D;
+		champTextures[2] = Resources.Load (FANNDIS_IMAGE_PATH) as Texture2D;
+		champTextures[3] = Resources.Load (KIRITO_IMAGE_PATH) as Texture2D;
+		champTextures[4] = Resources.Load (MERLINI_IMAGE_PATH) as Texture2D;
+		champTextures[5] = Resources.Load (TEMPTRESS_IMAGE_PATH) as Texture2D;
 		
 		confirm = Resources.Load (CONFIRM_IMAGE_PATH) as Texture2D;
 		confirmed = Resources.Load (CONFIRMED_IMAGE_PATH) as Texture2D;
+	}
+	
+	private void setChampNames() {
+		champNames[1] = "Albion";
+		champNames[2] = "Fanndis";
+		champNames[3] = "Kirito";
+		champNames[4] = "Merlini";
+		champNames[5] = "Temptress";
+	}
+	
+	private void setSkillsDescrip() {
+		champSkillsDescrip[1] = "Holy Trap - CD: 10 sec\n" +
+			"Albion places a trap on the map that is visible for 1 second " +
+			"by everyone, to stun whoever walks through it except the hero places the trap.\n" +
+			"Holy Blink - CD: 10 sec\n" +
+			"Albion teleports 3 squares to the front direction of the hero.";
 	}
 	
 	private void setControllers() {
 		for (int i = 1; i <= MAX_PLAYERS; i++) {
 			controllers[i].SetControllerNumber (i);
 			Debug.Log ("Controller" + (i) + " #: " + controllers[i].GetControllerNumber ());
+		}
+	}
+	
+	private void setDefaultChampInfo() {
+		// the first champ to shown when join the room
+		for (int i = 0; i <= MAX_PLAYERS; i++) {
+			currentSelectedIndex[i] = 0;
+			defaultChampTextureDisplayed[i] = champTextures[1];
+			defaultChampNameDisplayed[i] = champNames[1];
 		}
 	}
 	
@@ -158,24 +213,24 @@ public class ChampSelection : MonoBehaviour {
 	private void setGUIStyle() {
 		// font style
 		titleStyle.font = titleFont;
-		playerTabStyle.font = bodyFont;
+		playerTagStyle.font = bodyFont;
 		bodyStyle.font = bodyFont;
 		teamTagStyle.font = bodyFont;
 		
 		// font size
 		titleStyle.fontSize = 50;
-		playerTabStyle.fontSize = 20;
+		playerTagStyle.fontSize = 20;
 		bodyStyle.fontSize = 13;
 		teamTagStyle.fontSize = 15;
 		
 		// font color
 		titleStyle.normal.textColor = new Color(255f, 128f, 0f, 100f);
-		playerTabStyle.normal.textColor = Color.white;
+		playerTagStyle.normal.textColor = Color.white;
 		bodyStyle.normal.textColor = Color.white;
 		teamTagStyle.normal.textColor = Color.cyan;
 	}
 	
-	private void displayContents() {
+	private void displayLayout() {
 		// title
 		string title = "Champions Selection";
 		GUI.Label (new Rect(150f, 0f, 0f, 0f), "Select Champions", titleStyle);
@@ -183,28 +238,28 @@ public class ChampSelection : MonoBehaviour {
 		// Player 1 group
 		GUI.BeginGroup (new Rect(50f, 80f, 300f, 500f));  // make a group
 		
-		displayChampionContents (controllers[1], 1);
+		displayChampionContents (controllers[1], controllers[1].GetControllerNumber ());
 		
 		GUI.EndGroup ();  // end the group
 		
 		// Player 2 group
 		GUI.BeginGroup (new Rect(50f, 300f, 300f, 500f));  // make a group
 		
-		displayChampionContents (controllers[2], 2);
+		displayChampionContents (controllers[2], controllers[2].GetControllerNumber ());
 		
 		GUI.EndGroup ();  // end the group
 		
 		// Player 3 group
 		GUI.BeginGroup (new Rect(400f, 80f, 300f, 500f));  // make a group
 		
-		displayChampionContents (controllers[3], 3);
+		displayChampionContents (controllers[3], controllers[3].GetControllerNumber ());
 		
 		GUI.EndGroup ();  // end the group
 		
 		// Player 4 group
 		GUI.BeginGroup (new Rect(400f, 300f, 300f, 500f));  // make a group
 		
-		displayChampionContents (controllers[4], 4);
+		displayChampionContents (controllers[4], controllers[4].GetControllerNumber ());
 		
 		GUI.EndGroup ();  // end the group
 	}
@@ -215,26 +270,58 @@ public class ChampSelection : MonoBehaviour {
 		 * 1. No duplicates allowed
 		 */
 		
-		GUI.Label (new Rect(0f, 0f, 0f, 0f), "Player " + playerNum, playerTabStyle);  // player label
+		GUI.Label (new Rect(0f, 0f, 0f, 0f), "Player " + playerNum, playerTagStyle);  // player label
 		
-		displayChampTexture (controller);
+		displayChampInfo (controller, playerNum);
 		displayTeamInfo (controller);
 		displayConfirmationButton (controller);
 	}
 	
-	private void displayChampTexture(XInputController controller) {
-		Texture2D champBoxContent;
-		if (!isPlayerInRoom[controller.GetControllerNumber ()])
-			champBoxContent = champBox;
-		else {
-			champBoxContent = albionIcon;
-//			if (controller.GetThumbstick ("left").x > 0)
+	private void displayChampInfo(XInputController controller, int controllerNum) {
+		Texture2D champTextureBox = defaultChampTextureDisplayed[controllerNum];
+		string champNameLabel = defaultChampNameDisplayed[controllerNum];
+		string champSkillsBox;
+		
+		if (!isPlayerInRoom[controllerNum]) {
+			champTextureBox = champTextures[0];
+			champNameLabel = "";
 		}
-		GUI.Box (new Rect (0f, 30f, 100f, 100f), champBoxContent);
+		else {
+			if (navigateLeft[controllerNum]) {
+				int index = currentSelectedIndex[controllerNum];
+				index--;
+				
+				if (index < 1)
+					index = TOTAL_AVAILABLE_CHAMPS;
+				
+				currentSelectedIndex[controllerNum] = index;
+				
+				// update champion texture
+				defaultChampTextureDisplayed[controllerNum] = champTextures[index];
+				champTextureBox = defaultChampTextureDisplayed[controllerNum];
+				
+				defaultChampNameDisplayed[controllerNum] = champNames[index];
+				champNameLabel = defaultChampNameDisplayed[controllerNum];
+				
+				navigateLeft[controllerNum] = false;
+			}
+			else if (navigateRight[controllerNum]) {
+				int index = currentSelectedIndex[controllerNum];
+				index++;
+				
+				index = index % TOTAL_AVAILABLE_CHAMPS;
+				
+				
+			}
+		}
+		
+		GUI.Box (new Rect (0f, 30f, 100f, 100f), champTextureBox);
+		GUI.Label (new Rect(110f, 0f, 0f, 0f), champNameLabel, playerTagStyle);
+//		GUI.Box (new Rect(110f, 30f, 200f, 200f));
 	}
 	
 	private void displayTeamInfo(XInputController controller) {
-		GUI.Label (new Rect(0f, 125f, 100f, 50f), solo);
+		GUI.Label (new Rect(0f, 125f, 100f, 50f), teamTexture[0]);
 	}
 	
 	private void displayConfirmationButton(XInputController controller) {
@@ -246,6 +333,12 @@ public class ChampSelection : MonoBehaviour {
 		
 		GUI.Box (new Rect(0f, 150f, 100f, 25f), confirmContent);
 	}
+	
+//	private void displayChampName(int index) {
+//		Debug.Log ("Champ Name: " + champNames[index]);
+////		GUI.Label (new Rect(130f, 0f, 0f, 0f), champInfo.getChampName (index), playerTagStyle);
+//		GUI.Label (new Rect(130f, 0f, 0f, 0f), champNames[index], playerTagStyle);
+//	}
 	
 	private void setPlayerJoined() {
 		// don't need a loop to set this to waste time complexity
@@ -268,5 +361,47 @@ public class ChampSelection : MonoBehaviour {
 			confirmSelection[3] = true;
 		if (controllers[4].GetButtonPressed ("skill1") && isPlayerInRoom[4])
 			confirmSelection[4] = true;
+	}
+	
+	private void navigateState() {
+		// left
+		if (controllers[1].GetThumbstickDirectionOnce ("left") && isPlayerInRoom[1])
+			navigateLeft[1] = true;
+		if (controllers[2].GetThumbstick ("left").x < 0 && isPlayerInRoom[2])
+			navigateLeft[2] = true;
+		if (controllers[3].GetThumbstick ("left").x < 0 && isPlayerInRoom[3])
+			navigateLeft[3] = true;
+		if (controllers[4].GetThumbstick ("left").x < 0 && isPlayerInRoom[4])
+			navigateLeft[4] = true;
+		
+		// right
+		if (controllers[1].GetThumbstick ("left").x > 0 && isPlayerInRoom[1])
+			navigateRight[1] = true;
+		if (controllers[2].GetThumbstick ("left").x > 0 && isPlayerInRoom[2])
+			navigateRight[2] = true;
+		if (controllers[3].GetThumbstick ("left").x > 0 && isPlayerInRoom[3])
+			navigateRight[3] = true;
+		if (controllers[4].GetThumbstick ("left").x > 0 && isPlayerInRoom[4])
+			navigateRight[4] = true;
+		
+		// up
+		if (controllers[1].GetThumbstick ("left").y > 0 && isPlayerInRoom[1])
+			navigateUp[1] = true;
+		if (controllers[2].GetThumbstick ("left").x > 0 && isPlayerInRoom[2])
+			navigateUp[2] = true;
+		if (controllers[3].GetThumbstick ("left").x > 0 && isPlayerInRoom[3])
+			navigateUp[3] = true;
+		if (controllers[4].GetThumbstick ("left").x > 0 && isPlayerInRoom[4])
+			navigateUp[4] = true;
+		
+		// down
+		if (controllers[1].GetThumbstick ("left").y < 0 && isPlayerInRoom[1])
+			navigateDown[1] = true;
+		if (controllers[2].GetThumbstick ("left").x < 0 && isPlayerInRoom[2])
+			navigateDown[2] = true;
+		if (controllers[3].GetThumbstick ("left").x < 0 && isPlayerInRoom[3])
+			navigateDown[3] = true;
+		if (controllers[4].GetThumbstick ("left").x < 0 && isPlayerInRoom[4])
+			navigateDown[4] = true;
 	}
 }
