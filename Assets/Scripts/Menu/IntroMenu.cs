85f2eb82966d51b75eb583663b8617f88360f8fc
Drew Harvey <drew.harvey.10@gmail.com>
2013-06-07 14:45:16 -0700

    Added hover to home menu buttons

--- Assets/Scripts/Menu/IntroMenu.cs
+++ Assets/Scripts/Menu/IntroMenu.cs
@@ -5,15 +5,26 @@ public class IntroMenu : MonoBehaviour {
 	
 	public float menuWidth;
 	public float menuHeight;
-	private Texture2D menuBGTexture;
+	
+	private Texture2D menuBG;
+	private Texture2D menuBGHover;
 	private GUIStyle style;
 	
 	GUIContent[] menuItems;
 
 	// Use this for initialization
 	void Start () {
-		menuBGTexture = Resources.Load ("Textures/Menu/MenuButtonBG") as Texture2D;
-		
+		initMenu();
+	}
+	
+	void OnGUI() {
+		setMenuStyle();
+		buildMenu();
+	}
+	
+	private void initMenu() {
+		menuBG = Resources.Load ("Textures/Menu/MenuButtonBG") as Texture2D;	
+		menuBGHover = Resources.Load ("Textures/Menu/MenuButtonBGHover") as Texture2D;
 		menuItems = new GUIContent[5];
 		menuItems[0] = new GUIContent("Play", "play");
 		menuItems[1] = new GUIContent("Tutorial", "tutorial");
@@ -22,18 +33,18 @@ public class IntroMenu : MonoBehaviour {
 		menuItems[4] = new GUIContent("Quit", "quit");
 	}
 	
-	void OnGUI() {
+	private void setMenuStyle() {
 		// normal gui styles
 		GUI.skin.button.normal.textColor = Color.white;
 		GUI.skin.button.fontSize = 25;
-		GUI.skin.button.normal.background = menuBGTexture;
+		GUI.skin.button.normal.background = menuBG;
 		
 		// on hover gui styles
-		GUI.skin.button.hover.textColor = Color.blue;
-		GUI.skin.button.hover.background = menuBGTexture;
-		
-		// build menu
+		GUI.skin.button.hover.textColor = Color.white;
+		GUI.skin.button.hover.background = menuBGHover;
+	}
+	
+	private void buildMenu() {
 		GUI.SelectionGrid(new Rect(Screen.width / 2 - menuWidth / 2, Screen.height / 2 - menuHeight / 2, menuWidth, menuHeight * menuItems.Length), -1, menuItems, 1); 
-
 	}
 }
